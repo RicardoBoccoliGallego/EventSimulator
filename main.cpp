693a89a57a606648a612a51896cec93ac7f8ffbe
RicardoBoccoliGallego <ricardo.gallego93@gmail.com>
2015-04-25 17:12:32 -0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ * Arquivo principal: main.cpp
+ * Autor: Ricardo Boccoli Gallego
+ * Nusp:  8041992
+ */
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+
+using namespace std;
+
+void runTests() {
+
+}
+
+int main ()
+{
+   runTests();
+
+    return 0;
+}
