51a13691c2c763419ed48d129259a6f4d3f40899
RicardoBoccoliGallego <ricardo.gallego93@gmail.com>
2015-06-03 18:47:51 -0300

    Configuração Windows

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -15,19 +15,19 @@ Memory::Memory(int64_t size)
 void Memory::RequestMemory(const Job* job) {
 	if (_memory_size >= job->NeededMemory()) {
 		_memory_size -= job->NeededMemory();
-		_alocated_jobs.insert(job);
+		//_alocated_jobs.insert(job);
 	}
-	else
-		_jobs_queue.push_back(job);
+//	else
+		//_jobs_queue.push_back(job);
 }
 
 void Memory::ReleaseMemory(const Job* job) {
 	if (true) {
 		_memory_size += job->NeededMemory();
-		if (!_jobs_queue.empty() && _jobs_queue.front()->NeededMemory() <= _memory_size) {
-			_alocated_jobs.insert(*_jobs_queue.front());
-			_jobs_queue.pop_front();
-		}
-		_alocated_jobs.erase(job);
+	//	if (!_jobs_queue.empty() && _jobs_queue.front()->NeededMemory() <= _memory_size) {
+		//	_alocated_jobs.insert(*_jobs_queue.front());
+	//		_jobs_queue.pop_front();
+//		}
+	//	_alocated_jobs.erase(job);
 	}
 }
